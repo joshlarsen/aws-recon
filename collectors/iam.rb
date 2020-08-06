ce88f302cde45b82f288ee0444501f0b7f404d14
Josh Larsen <jlarsen@gmail.com>
2020-08-06 10:45:53 -0400

    virtual mfa device hash

--- collectors/iam.rb
+++ collectors/iam.rb
@@ -1,6 +1,6 @@
 class IAM < Mapper
   #
-  # Returns an array of resources. 
+  # Returns an array of resources.
   #
   def collect
     resources = []
@@ -12,11 +12,11 @@ class IAM < Mapper
     #
     @client.get_account_authorization_details.each_with_index do |response, page|
       log(response.context.operation_name, page)
-      
+
       # users
       response.user_detail_list.each do |user|
         struct = OpenStruct.new(user.to_h)
-        struct.type = "user"
+        struct.type = 'user'
         struct.mfa_devices = @client.list_mfa_devices({ user_name: user.user_name }).mfa_devices
         struct.ssh_keys = @client.list_ssh_public_keys({ user_name: user.user_name }).ssh_public_keys
 
@@ -26,7 +26,7 @@ class IAM < Mapper
       # groups
       response.group_detail_list.each do |group|
         struct = OpenStruct.new(group.to_h)
-        struct.type = "group"
+        struct.type = 'group'
 
         resources.push(struct.to_h)
       end
@@ -34,7 +34,7 @@ class IAM < Mapper
       # roles
       response.role_detail_list.each do |role|
         struct = OpenStruct.new(role.to_h)
-        struct.type = "role"
+        struct.type = 'role'
 
         resources.push(struct.to_h)
       end
@@ -42,7 +42,7 @@ class IAM < Mapper
       # polices
       response.policies.each do |policy|
         struct = OpenStruct.new(policy.to_h)
-        struct.type = "policy"
+        struct.type = 'policy'
 
         resources.push(struct.to_h)
       end
@@ -50,12 +50,12 @@ class IAM < Mapper
 
     #
     # get_account_password_policy
-    #    
+    #
     @client.get_account_password_policy.each do |response|
       log(response.context.operation_name)
 
       struct = OpenStruct.new(response.password_policy.to_h)
-      struct.type = "password_policy"
+      struct.type = 'password_policy'
 
       resources.push(struct.to_h)
     end
@@ -67,7 +67,7 @@ class IAM < Mapper
       log(response.context.operation_name)
 
       struct = OpenStruct.new(response.summary_map)
-      struct.type = "account_summary"
+      struct.type = 'account_summary'
 
       resources.push(struct.to_h)
     end
@@ -80,12 +80,11 @@ class IAM < Mapper
 
       response.server_certificate_metadata_list.each do |cert|
         struct = OpenStruct.new(cert)
-        struct.type = "server_certificate"
+        struct.type = 'server_certificate'
         struct.arn = cert.arn
 
         resources.push(struct.to_h)
       end
-
     end
 
     #
@@ -95,8 +94,8 @@ class IAM < Mapper
       log(response.context.operation_name, page)
 
       response.virtual_mfa_devices.each do |mfa_device|
-        struct = OpenStruct.new(mfa_device)
-        struct.type = "virtual_mfa_device"
+        struct = OpenStruct.new(mfa_device.to_h)
+        struct.type = 'virtual_mfa_device'
         struct.arn = mfa_device.serial_number
 
         resources.push(struct.to_h)
