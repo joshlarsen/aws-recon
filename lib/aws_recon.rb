f3e6cca23acbe1ea322c47bf24b14197f393d415
Josh Larsen <jlarsen@gmail.com>
2020-08-12 09:12:19 -0400

    resolve circular include

--- lib/aws_recon.rb
+++ lib/aws_recon.rb
@@ -12,7 +12,7 @@ require 'aws-sdk'
 require 'aws_recon/options.rb'
 require 'aws_recon/lib/mapper.rb'
 require 'aws_recon/lib/formatter.rb'
-require 'aws_recon/collectors/collectors.rb'
+require 'aws_recon/collectors.rb'
 
 require 'aws_recon/version'
 require 'aws_recon/aws_recon'
