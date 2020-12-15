2c1e11d5e93d14afdb45a0e621d98361e7b8c355
Josh Larsen <jlarsen@gmail.com>
2020-12-15 06:55:27 -0500

    Handle queue with no policy. Fixes #77.

--- lib/aws_recon/collectors/sqs.rb
+++ lib/aws_recon/collectors/sqs.rb
@@ -18,7 +18,7 @@ class SQS < Mapper
         struct = OpenStruct.new(@client.get_queue_attributes({ queue_url: queue, attribute_names: ['All'] }).attributes.to_h)
         struct.type = 'queue'
         struct.arn = struct.QueueArn
-        struct.policy = struct.delete_field('Policy').parse_policy
+        struct.policy = struct.Policy ? struct.delete_field('Policy').parse_policy : nil
 
         resources.push(struct.to_h)
       end
