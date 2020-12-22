1835e930fa7a8dd797f4474a7ee53bc154d434a0
Josh Larsen <jlarsen@gmail.com>
2020-12-22 15:51:05 -0500

    Collect more SageMaker details

--- lib/aws_recon/collectors/sagemaker.rb
+++ lib/aws_recon/collectors/sagemaker.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+#
+# Collect SageMaker Resources
+#
 class SageMaker < Mapper
   #
   # Returns an array of resources.
@@ -12,9 +17,28 @@ class SageMaker < Mapper
       log(response.context.operation_name, page)
 
       response.notebook_instances.each do |instance|
-        struct = OpenStruct.new(instance.to_h)
+        struct = OpenStruct.new(@client.describe_notebook_instance({
+                                                                     notebook_instance_name: instance.notebook_instance_name
+                                                                   }).to_h)
         struct.type = 'notebook_instance'
         struct.arn = instance.notebook_instance_arn
+
+        resources.push(struct.to_h)
+      end
+    end
+
+    #
+    # list_endpoints
+    #
+    @client.list_endpoints.each_with_index do |response, page|
+      log(response.context.operation_name, page)
+
+      response.endpoints.each do |instance|
+        struct = OpenStruct.new(@client.describe_endpoint({
+                                                            endpoint_name: instance.endpoint_name
+                                                          }).to_h)
+        struct.type = 'endpoint'
+        struct.arn = instance.endpoint_arn
 
         resources.push(struct.to_h)
       end
