4faeaa2364cdb9706a4fbac3d3c339fd7636968f
Josh Larsen <jlarsen@gmail.com>
2021-04-25 16:56:58 -0400

    better lambda parsing

--- lib/aws_recon/collectors/lambda.rb
+++ lib/aws_recon/collectors/lambda.rb
@@ -17,6 +17,9 @@ class Lambda < Mapper
         struct = OpenStruct.new(function)
         struct.type = 'function'
         struct.arn = function.function_arn
+        struct.vpc_config = function.vpc_config.to_h
+        struct.tracing_config = function.tracing_config.to_h
+        struct.layers = function.layers ? function.layers.map(&:to_h) : []
         struct.policy = @client.get_policy({ function_name: function.function_name }).policy.parse_policy
 
       rescue Aws::Lambda::Errors::ResourceNotFoundException => e
@@ -36,6 +39,7 @@ class Lambda < Mapper
         struct = OpenStruct.new(layer)
         struct.type = 'layer'
         struct.arn = layer.layer_arn
+        struct.latest_matching_version = layer.latest_matching_version.to_h
 
         # list_layer_versions
         struct.versions = @client.list_layer_versions({ layer_name: layer.layer_name }).layer_versions.map(&:to_h)
