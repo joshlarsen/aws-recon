a23cc597b2854dea2b78fa9c91431cec1c364c7d
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2022-04-07 22:05:45 +0100

    Add ECRPublic support

---
+++ lib/aws_recon/collectors/ecrpublic.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+#
+# Collect ECRPublic resources
+#
+class ECRPublic < Mapper
+  #
+  # Returns an array of resources.
+  #
+  def collect
+    resources = []
+
+    #
+    # describe_repositories
+    #
+
+    puts(@client.describe_repositories)
+    @client.describe_repositories.each_with_index do |response, page|
+      log(response.context.operation_name, page)
+
+      response.repositories.each do |repo|
+        struct = OpenStruct.new(repo.to_h)
+        struct.type = "repository"
+        struct.arn = repo.repository_arn
+
+        struct.images = []
+        #
+        # describe images
+        #
+        puts(@client.describe_images({ repository_name: repo.repository_name }))
+        @client.describe_images({ repository_name: repo.repository_name }).image_details.each_with_index do |image, page|
+          log(response.context.operation_name, "list_images", page)
+          image_hash = image.to_h
+          struct.images << image_hash
+        end
+      rescue Aws::ECR::Errors::ServiceError => e
+        log_error(e.code)
+
+        raise e unless suppressed_errors.include?(e.code) && !@options.quit_on_exception
+      ensure
+        resources.push(struct.to_h)
+      end
+    end
+
+    resources
+  end
+
+  private
+
+  # not an error
+  def suppressed_errors
+    %w[
+      RepositoryPolicyNotFoundException,
+      ScanNotFoundException
+    ]
+  end
+end
